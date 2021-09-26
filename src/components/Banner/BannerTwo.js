1b1500c85a06db61a4994725e1ab9e2a6dcfbb13
valerc123 <valentina.rua123@gmail.com>
2021-09-26 14:17:44 -0500

    img

--- src/components/Banner/BannerTwo.js
+++ src/components/Banner/BannerTwo.js
@@ -1,41 +1,55 @@
 import Link from "next/link";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const BannerTwo = () => {
   return (
     <div className="banner-area space-pb--r70">
       <Container>
         <Row>
-          <Col md={6}>
+          <Col md={4}>
             <div className="single-banner">
+              <div className="single-banner__info">
+                <h3 className="title">Laboratory</h3>
+                <Button>More Info</Button>
+                {/* <Link href="/shop/grid-left-sidebar">
+                  <a className="link">More info</a>
+                </Link> */}
+              </div>
               <img
-                src="/assets/images/banner/shop_banner_img1.jpg"
+                src="/assets/images/banner/Untitled-1.png"
                 alt="shop_banner_img1"
               />
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="single-banner">
               <div className="single-banner__info">
-                {/* <h5>Super Sale</h5> */}
                 <h3 className="title">Cultivation</h3>
                 <Link href="/shop/grid-left-sidebar">
                   <a className="link">More info</a>
                 </Link>
               </div>
-            </div>
-          </Col>
-          <Col md={6}>
-            <div className="single-banner">
               <img
-                src="/assets/images/banner/shop_banner_img2.jpg"
+                src="/assets/images/banner/ej1.png"
                 alt="shop_banner_img1"
               />
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="single-banner">
               <div className="single-banner__info">
-                {/* <h5>New Season</h5> */}
                 <h3 className="title">Laboratory</h3>
                 <Link href="/shop/grid-left-sidebar">
                   <a className="link">More info</a>
                 </Link>
               </div>
+              <img
+                src="/assets/images/banner/ej2.png"
+                alt="shop_banner_img1"
+              />
             </div>
           </Col>
+          
         </Row>
       </Container>
     </div>
